95c76cdeca4d2338e9490236e8ef4a0b96accdfe
Andrey <75383288+Andrey529@users.noreply.github.com>
2022-03-08 22:54:15 +0300

    fix bug with const parametr in constructor dataPair

--- 4sem/Lab1_Associative_array_(map)/headers/dataPair/dataPair.h
+++ 4sem/Lab1_Associative_array_(map)/headers/dataPair/dataPair.h
@@ -1,12 +1,12 @@
 #ifndef LAB1_ASSOCIATIVE_ARRAY__MAP__DATAPAIR_H
 #define LAB1_ASSOCIATIVE_ARRAY__MAP__DATAPAIR_H
 
-template<class T_key, typename T_value>
+template<class T_key, class T_value>
 class dataPair{
 public:
     const T_key key;
     T_value value;
-    dataPair(const T_key &key, T_value &value) : key(key), value(value) {}
+    dataPair(const T_key &key, const T_value &value) : key(key), value(value) {}
     ~dataPair() = default;
 };
 
