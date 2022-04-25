f4dd112315e02e48e31a6b7b5ae957f606449579
Andrey529 <andr02eyes@gmail.com>
2022-04-25 20:23:40 +0300

    added comments

--- 4sem/Lab1_Associative_array_(map)/headers/AssociativeArray/AssociativeArray.h
+++ 4sem/Lab1_Associative_array_(map)/headers/AssociativeArray/AssociativeArray.h
@@ -14,17 +14,62 @@ public:
     AssociativeArray(const T_key &key, const T_value &value);
     ~AssociativeArray() = default;
 
+    /**
+     * Сhecks if an associative array is empty
+     * @return true if empty, false if not empty
+     */
     bool isEmpty() const;
-    void insert(const T_key &key, const T_value &value); // добавление элемента с ключом и значением
+
+    /**
+    * Inserts value by key
+    * @param key
+    * @param value
+    */
+    void insert(const T_key &key, const T_value &value);
+
+    /**
+     * Returns an iterator to width
+     */
     std::unique_ptr<Iterator<std::shared_ptr<ElemOfRedBlackTree<T_key, T_value>>>> createDftIterator();
 
-    void remove(const T_key &key); // удаление элемента по ключу
-    std::shared_ptr<ElemOfRedBlackTree<T_key, T_value>> find(const T_key &key) const; // поиск элемента по ключу
+    /**
+     * Removes the element with key = key
+     * @param key
+     */
+    void remove(const T_key &key);
+
+    /**
+     * Finding an element by a key equal to key
+     * @param key
+     * @return shared pointer to founded element
+     */
+    std::shared_ptr<ElemOfRedBlackTree<T_key, T_value>> find(const T_key &key) const;
+
+    /**
+    * Checks if there is an element with a key equal to key
+    * @param key
+    */
     bool contains(const T_key &key);
-    void clear(); // очищение ассоциативного массива
-    List<T_key> getKeys(); // возвращает список ключей
-    List<T_value> getValues(); // возвращает список значений
-    void print(); // вывод в консоль
+
+    /**
+     * Clears an associative array leaving it empty
+     */
+    void clear();
+
+    /**
+     * Returns a list of all keys bypassing the associative array in width
+     */
+    List<T_key> getKeys();
+
+    /**
+     *  Returns a list of all values bypassing the associative array in width
+     */
+    List<T_value> getValues();
+
+    /**
+     * Prints {key : value} pairs to the console
+     */
+    void print();
 };
 
 #include "../../src/AssociativeArray/AssociativeArray.cpp"
--- 4sem/Lab1_Associative_array_(map)/headers/RedBlackTree/RedBlackTree.h
+++ 4sem/Lab1_Associative_array_(map)/headers/RedBlackTree/RedBlackTree.h
@@ -22,17 +22,62 @@ public:
     RedBlackTree() : head_(nullptr) {}
     ~RedBlackTree() = default;
 
+    /**
+     * Сhecks if a red black tree is empty
+     * @return true if empty, false if not empty
+     */
     bool isEmpty() const;
-    void insert(const T_key &key, const T_value &value); // добавление элемента с ключом и значением
+
+    /**
+     * Inserts value by key
+     * @param key
+     * @param value
+     */
+    void insert(const T_key &key, const T_value &value);
+
+    /**
+     * Returns an iterator to width
+     */
     std::unique_ptr<Iterator<std::shared_ptr<ElemOfRedBlackTree<T_key, T_value>>>> createDftIterator();
 
-    void remove(const T_key &key); // удаление элемента по ключу
-    std::shared_ptr<ElemOfRedBlackTree<T_key, T_value>> find(const T_key &key) const; // поиск элемента по ключу
+    /**
+     * Removes the element with key = key
+     * @param key
+     */
+    void remove(const T_key &key);
+
+    /**
+     * Finding an element by a key equal to key
+     * @param key
+     * @return shared pointer to founded element
+     */
+    std::shared_ptr<ElemOfRedBlackTree<T_key, T_value>> find(const T_key &key) const;
+
+    /**
+     * Checks if there is an element with a key equal to key
+     * @param key
+     */
     bool contains(const T_key &key);
-    void clear(); // очищение ассоциативного массива
-    List<T_key> getKeys(); // возвращает список ключей
-    List<T_value> getValues(); // возвращает список значений
-    void print(); // вывод в консоль
+
+    /**
+     * Clears an red black tree leaving it empty
+     */
+    void clear();
+
+    /**
+     * Returns a list of all keys bypassing the red black tree in width
+     */
+    List<T_key> getKeys();
+
+    /**
+     * Returns a list of all values bypassing the red black tree in width
+     */
+    List<T_value> getValues();
+
+    /**
+     * Prints {key : value} pairs to the console
+     */
+    void print();
 };
 
 
